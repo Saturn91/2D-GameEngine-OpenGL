b92a6fba2c96ea52682d3708654605167fef007d
Saturn91 <manuel.geissberger@gmail.com>
2016-07-09 18:58:04 +0200

    [TEST] transparende in layer 2 -> works

--- 2D_Engine_LWJGL/src/game/level/Map.java
+++ 2D_Engine_LWJGL/src/game/level/Map.java
@@ -20,11 +20,19 @@ public class Map {
 		TileSet tileset = new TileSet("Standart", "TestTileSet", 32, 32);
 		Loader loader = new Loader();
 		
+		RawModel model= loader.loadToVAO(Constants.QuadVerticies(1, 1), Constants.TextureCords(), Constants.QuadIndices());
+		ModelTexture texture = new ModelTexture(loader.loadTexture("transparence"));
+		TexturedModel staticModel = new TexturedModel(model, texture);
+		Entity entity = new Entity(staticModel);
+		GameObject.addEntity("Test", entity);
+		
 		for(int x = 0; x < 25; x++){
 			for(int y = 0; y < 25; y++){
-				Game.addEntity(tileset.getTile(0, 1, new Vector2f(x, y), 1, 1));
+				Game.addEntity(tileset.getTile(0, 0, new Vector2f(x, y), 1, 0));
 			}
-		}		
+		}
+		
+		Game.addEntity(new GameObject("Test", new Vector2f(0,0), 1.5f, 1));
 	}
 	
 	public void cleanUp(){
